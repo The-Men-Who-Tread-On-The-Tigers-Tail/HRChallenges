3139a7ac81ef8e51338fe35b29af3684de75d396
Josef Eisvogel <josef@Wojtek.animalfarm.home>
2017-06-06 17:27:54 +0200

    hello world completed

---
+++ 30_days_of_code/day_0_hello_world/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+    
+            Scanner sc = new Scanner(System.in);
+        
+            String line;
+            
+            line = sc.nextLine();
+            
+            System.out.println("Hello, World.");
+            System.out.println(line);
+        }
+}
